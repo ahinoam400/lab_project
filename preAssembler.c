5ed2650f9cef10e7197d0312d9da33069db12738
ahinoam400 <ahinoam400@gmail.com>
2022-02-08 20:51:54 +0200

    commit

--- preAssembler.c
+++ preAssembler.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include <string.h>
 int MAX = 80;
-int preAssembler(char* assemblyFile);
+int preAssembler(char* assemblyFileName);
 
 int main(){
     preAssembler("assemblyExample.as");
 }
 
-int preAssembler(char* assemblyFile){
-    FILE *assembly = fopen(assemblyFile, "r");
+int preAssembler(char* assemblyFileName){
+    FILE *assembly = fopen(assemblyFileName, "r");
     FILE *afteAssembler;
+    char *after;
     char str[80];
     if(assembly == NULL){
         printf("Error opening file\n");
         return(-1);
     }
+    strcpy(after, assemblyFileName);
+    strcat(after, ".am");
 }
