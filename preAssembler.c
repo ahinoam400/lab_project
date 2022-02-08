2b40bf9a898aa87956aec5541d64616a2f99c211
ahinoam400 <ahinoam400@gmail.com>
2022-02-08 21:19:02 +0200

    commit

--- preAssembler.c
+++ preAssembler.c
@@ -3,6 +3,7 @@
 #include <string.h>
 int MAX = 80;
 int preAssembler(char* assemblyFileName);
+char *removeExtension(char* myStr);
 
 int main(){
     preAssembler("assemblyExample.as");
@@ -11,13 +12,26 @@ int main(){
 int preAssembler(char* assemblyFileName){
     FILE *assembly = fopen(assemblyFileName, "r");
     FILE *afteAssembler;
-    char after[20];
+    char *after;
     char str[80];
     if(assembly == NULL){
         printf("Error opening file\n");
         return(-1);
     }
-    strcpy(after, assemblyFileName);
+    after = removeExtension(assemblyFileName);
     strcat(after, ".am");
     afteAssembler = fopen(after, "w");
+}
+
+//remove the extension from a file name
+char *removeExtension(char* myStr) {
+    char *retStr;
+    char *lastExt;
+    if (myStr == NULL) return NULL;
+    if ((retStr = malloc (strlen (myStr) + 1)) == NULL) return NULL;
+    strcpy (retStr, myStr);
+    lastExt = strrchr (retStr, '.');
+    if (lastExt != NULL)
+        *lastExt = '\0';
+    return retStr;
 }
