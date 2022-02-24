266210a6df8ceefdab09b08bf2794d6319cada5c
ahinoam400 <ahinoam400@gmail.com>
2022-02-24 10:54:19 +0200

    commit

--- structs.h
+++ structs.h
@@ -1,16 +1,56 @@
-#include "constant.h"
 
-typedef struct{/*struct for the operation*/
-    char name[5];
+
+typedef struct{/*the first word*/
     int opcode : 16;
-    int funct : 4;
-}operation;
+    int coding_class : 3;/*A=Absolute, R=Relocatable or E=External*/
+    int empty_bit : 1;
+}cmd_word;
 
-operation operationsArr[MAX_CMD_NUM]{
-    {"mov",0,0}, {"cmp",1,0}, {"add",2,10},
-    {"sub",2,11}, {"lea",4,0}, {"clr",5,10},
-    {"not",5,11}, {"inc",5,12}, {"dec",5,13},
-    {"jmp",9,10}, {"bne",9,11}, {"jsr",9,12},
-    {"red",12,0}, {"prn",13,0}, {"rts",14,0},
-    {"stop",15,0}
-};
+typedef struct{/*for comands with operands*/
+    int dest_address : 2;
+    int dest_register : 4;
+    int src_address : 2;
+    int src_register : 4;
+    int funct : 4;
+    int coding_class : 3;/*A=Absolute, R=Relocatable or E=External*/
+    int empty_bit : 1;
+}second_word;
+
+typedef struct{/*word for immediate addressing mode*/
+    int word : 16;
+    int coding_class : 3;/*always A=Absolute*/
+    int empty_bit : 1;
+}immediate_word;
+
+typedef struct{/*two words for direct addressing mode*/
+    /*the first word:*/
+    unsigned int base_address : 16;
+    int coding_class : 3;/*R=Relocatable or E=External*/
+    int empty_bit : 1;
+    /*the second word:*/
+    unsigned int offset :16;
+    int coding_class : 3;/*R=Relocatable or E=External*/
+    int empty_bit : 1;
+}direct_words;
+
+typedef struct{/*two words for index addressing mode*/
+    /*the first word:*/
+    unsigned int base_address : 16;
+    int coding_class : 3;/*R=Relocatable or E=External*/
+    int empty_bit : 1;
+    /*the second word:*/
+    unsigned int offset :16;
+    int coding_class : 3;/*R=Relocatable or E=External*/
+    int empty_bit : 1;
+}index_words;
+
+typedef union{/*hold the code for a line*/
+    cmd_word command;
+    second_word word; /*for comands with operands*/
+    immediate_word imm_word; /*for immediate addressing mode*/
+    direct_words dir_words; /*for direct addressing mode*/
+    index_words inx_word; /*for index addressing mode*/
+}code;
+
+
+
