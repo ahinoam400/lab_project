afb3d9bbcb90c63dc7c88aa8ac11e2c7d99c5be1
ahinoam400 <ahinoam400@gmail.com>
2022-02-24 10:54:23 +0200

    commit

--- constant.h
+++ constant.h
@@ -1,3 +1,18 @@
 
 #define MAX_LINE_LEN 80
 #define MAX_CMD_NUM 16
+
+typedef struct{/*struct for the comands*/
+    char cmdName[5];
+    int cmd_opcode;
+    int cmd_funct;
+}comand;
+
+comand cmd_arr[MAX_CMD_NUM]{
+    {"mov",0,0}, {"cmp",1,0}, {"add",2,10},
+    {"sub",2,11}, {"lea",4,0}, {"clr",5,10},
+    {"not",5,11}, {"inc",5,12}, {"dec",5,13},
+    {"jmp",9,10}, {"bne",9,11}, {"jsr",9,12},
+    {"red",12,0}, {"prn",13,0}, {"rts",14,0},
+    {"stop",15,0}
+};
--- firstPass.c
+++ firstPass.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
-#include "firstPass.h"
+#include "constant.h"
+#include "stracts.h"
 long int DecimalToBinary(int n);
+
 
 int main(){
     printf("%ld", DecimalToBinary(8));
 }
+
+/*this function get a line, turns on the flag if there is a label,
+ calculates how many words are needed in memory (according to the 
+ command and according to the operands) and puts the line in memory*/
+int lineProcess(char* line){
+
+}
+
 /*convert decimal number to binary number*/
 long int DecimalToBinary(int decNum){
     int binaryNum[16];
