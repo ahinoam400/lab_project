f337619d68a2d398de93aa29f17deb279b8a517c
AmirWolff <amirwo218@gmial.com>
2022-03-26 10:06:05 -0700

    commit

--- fileMaker.c
+++ fileMaker.c
@@ -30,42 +30,6 @@ void objectFile(struct images *images, char *fileName, int ICF, int DCF){
     FILE *objectF = fopen(strcat(fileNameCopy, ".ob"), "a");
     if (objectF == NULL)
         return (printAndReturn("ERROR OPENING FILE\n", -1));
-    char str[MAX_LINE_LEN];
-    code *c = (code*)malloc(sizeof(code));
-    c = images->code_head;
-    char temp;
-    int optemp;
-    char title[MAX_LINE_LEN];
-    strcat(title, ICF);
-    strcat(title, " ");
-    strcat(title, DCF);
-    fwrite(title, 1, sizeof(str), objectF);
-    while(c != NULL){
-        if(c->code_line->command != NULL){
-            strcat(str, "A");
-            if(c->code_line->command->absolute == 1) strcat(str, "4");
-            else if(c->code_line->command->relocatable == 1) strcat(str, "2");
-            else strcat(str, "1");
-            temp = decToHex(c->code_line->command->opcode>>12);
-            strcat(str, "-B");
-            strcat(str, temp);
-            optemp = temp;
-            temp = optemp - decToHex(c->code_line->command->opcode>>8);
-            strcat(str, "-C");
-            strcat(str, temp);
-            optemp += temp;
-            temp = optemp - decToHex(c->code_line->command->opcode>>4);
-            strcat(str, "-D");
-            strcat(str, temp);
-            optemp += temp;
-            temp = optemp - decToHex(c->code_line->command->opcode);
-            strcat(str, "-E");
-            strcat(str, temp);
-            fwrite(str, 1, sizeof(str), objectF);
-            str[0] = '\0';
-        }
-        c = c->next;
-    }
 }
 
 int decToHex(int decNumber){
