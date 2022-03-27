a76c1b7b692c295aad926d2322850fe966f4786e
AmirWolff <amirwo218@gmial.com>
2022-03-27 10:59:22 -0700

    commit

--- firstPass.h
+++ firstPass.h
@@ -3,7 +3,3 @@ int isLegalSymName(char symbolName[MAX_LINE_LEN]);
 int isLegalNumber(char *number);
 int isRegister(char *str);
 int firstPass(const char *filename, struct images *images);
-
-data* addDataNode(data *tail, int lineNum);
-code* addCodeNode(code *tail, int lineNum);
-symbol *addSymbolNode(symbol *tail, int lineNum);
